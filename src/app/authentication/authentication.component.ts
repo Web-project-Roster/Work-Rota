2459ae1f5bd78ee4bdef32114da56f70a992ef52
ConorDoherty1998 <Conordoherty980@gmail.com>
2019-11-06 20:12:46 +0000

    configured routes for form section + added background svg

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-authentication',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authentication.component.scss']
 })
 export class AuthenticationComponent implements OnInit {
+  // @ViewChild('gradientBackground', {static: false}) gradientWindow: ElementRef;
 
-  constructor() { }
+  tabsForm = new FormGroup({
+    formTab: new FormControl()
+  });
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, private router: Router) { 
+    this.formTab.valueChanges.subscribe(
+      (value: any) => { this.router.navigate(['auth/' + value]) }
+    )
   }
 
+  ngOnInit() {
+    this.tabsForm.setValue({formTab: 'login'})
+  }
+
+  // ngAfterViewInit() {
+  //   console.log(this.gradientWindow)
+  //   const ctx = this.gradientWindow.nativeElement.getContext("2d")
+  //   let grd = ctx.createLinearGradient(0,0,200,0)
+  //   grd.addColorStop(0,'red')
+  //   grd.addColorStop(1,'green')
+  //   ctx.fillStyle = grd
+  //   ctx.fillRect(10,10,150,80)
+  // }
+
+  get formTab() { return this.tabsForm.get('formTab') }
+ 
 }
