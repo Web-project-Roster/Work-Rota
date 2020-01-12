f841a457d3219e5eb00b06fc281227c7c5efefa4
ConorDoherty1998 <Conordoherty980@gmail.com>
2020-01-12 15:36:33 +0000

    small animation for selecting rota

--- src/app/pages/rota/rota-tile/rota-tile.component.ts
+++ src/app/pages/rota/rota-tile/rota-tile.component.ts
@@ -1,19 +1,39 @@
 import { Component, OnInit, Input, HostListener, Output, EventEmitter } from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations';
 import { Router } from '@angular/router';
 import { ViewRotaService } from 'src/app/view-rota.service';
 
 @Component({
   selector: 'app-rota-tile',
   templateUrl: './rota-tile.component.html',
-  styleUrls: ['./rota-tile.component.scss']
+  styleUrls: ['./rota-tile.component.scss'],
+  animations: [
+    trigger('clickTile', [
+      state('unset', style({})),
+      state('right', style({
+        transform: 'translatex(20px)'
+      })),
+      state('up', style({
+        transform: 'translatey(10px)'
+      })),
+      transition('unset => right', animate('400ms ease-out')),
+      transition('right => unset', animate('400ms ease-out')),
+      transition('unset => up', animate('400ms ease-out')),
+      transition('up => unset', animate('400ms ease-out'))
+    ])
+  ]
 })
 export class RotaTileComponent implements OnInit {
   @Input() rota;
+  isSelected = false
 
   //A click event cannot be assigned to a component directly, so a HostListener must be used instead.
   @HostListener("click") onClick(){
     this.router.navigate(['/rota', { outlets: {'rota-grid': 'edit', 'rota-shelf-left': 'view' }}])
     this.viewRotaService.selectedRota.setValue(this.rota)
+    this.viewRotaService.selectedRota.valueChanges.subscribe((value:any) =>  {
+      value.sk == this.rota.sk ? this.isSelected = true : this.isSelected = false
+    })
   }
 
   constructor(private viewRotaService: ViewRotaService, private router: Router) { }
@@ -21,4 +41,9 @@ export class RotaTileComponent implements OnInit {
   ngOnInit() {
   }
 
+  get getState() {
+    if (window.screen.width > 990)
+      return this.isSelected ? 'right' : 'unset'
+    return this.isSelected ? 'up' : 'unset'
+  }
 }
